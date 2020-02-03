56e5a3ec1075eaa018167bebe2e2e91a67d08892
KwesiD <kfd233@nyu.edu>
2020-02-02 23:26:47 -0500

    Added working arduino code for server/probe

--- Ultrasound/Ultrasound.ino
+++ Ultrasound/Ultrasound.ino
@@ -14,6 +14,14 @@
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <WiFiAP.h>
+ 
+#include <SPI.h>
+#include <MFRC522.h>
+#include <String.h> 
+
+#define RFID_SS  21//10
+#define RFID_RST 22//9
+MFRC522 rfid( RFID_SS, RFID_RST );
 
 // Set these to your desired credentials.
 const char *ssid = "SimuSound";
@@ -23,8 +31,9 @@ WiFiServer server(80);
 
 
 void setup() {
-
+  SPI.begin();
   Serial.begin(115200);
+  rfid.begin();
   Serial.println();
   Serial.println("Configuring access point...");
 
@@ -40,7 +49,10 @@ void setup() {
 
 void loop() {
   WiFiClient client = server.available();   // listen for incoming clients
-
+  String uid = getUID();
+  if(!uid.equals("")){
+    Serial.println(uid);
+  }
   if (client) {                             // if you get a client,
     //Serial.println("New Client.");           // print a message out the serial port
     String currentLine = "";                // make a String to hold incoming data from the client
@@ -56,10 +68,11 @@ void loop() {
             // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
             // and a content-type so the client knows what's coming, then a blank line:
             client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
+            client.println("Content-type:text/plain");
+            client.println("charset=utf-8");
             client.println();
 
-            client.print("");
+            client.print(uid);
             // the content of the HTTP response follows the header:
 //            client.print("Click <a href=\"/H\">here</a> to turn ON the LED.<br>");
 //            client.print("Click <a href=\"/L\">here</a> to turn OFF the LED.<br>");
@@ -74,20 +87,32 @@ void loop() {
         } else if (c != '\r') {  // if you got anything else but a carriage return character,
           currentLine += c;      // add it to the end of the currentLine
         }
-
-        // Check to see if the client request was "GET /H" or "GET /L":
-        if (currentLine.endsWith("GET /H")) {
-          Serial.println("High");
-          //digitalWrite(LED_BUILTIN, HIGH);               // GET /H turns the LED on
-        }
-        if (currentLine.endsWith("GET /L")) {
-          Serial.println("Low");
-          //digitalWrite(LED_BUILTIN, LOW);                // GET /L turns the LED off
-        }
+        
       }
     }
     // close the connection:
     client.stop();
-    Serial.println("Client Disconnected.");
+    //Serial.println("Client Disconnected.");
   }
+}
+
+String getUID(){
+  byte data[MAX_LEN];
+  byte uid[5];
+  //String hexValues[5];
+  String hex = "";
+  if ( rfid.requestTag( MF1_REQIDL, data ) == MI_OK ) {
+    if ( rfid.antiCollision( data ) == MI_OK ) {
+      memcpy( uid, data, 5 );
+      //return uid;
+      for ( int i = 0; i < 5; i++ ) {
+        hex += String(uid[i],HEX);
+      }
+//      for(int i = 0;i < 5;i++){
+//        hex += hexValues[i];
+//      }
+      return hex;
+    }
+  }
+  return "";
 }
